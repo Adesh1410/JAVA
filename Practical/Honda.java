ea560ce9379f9cbc72f2ef06b2f57814386dec79
Adesh1410 <123162694+Adesh1410@users.noreply.github.com>
2023-03-01 18:15:29 +0530

    Add files via upload

---
+++ Practical/Honda.java
@@ -0,0 +1,18 @@
+package my;
+
+abstract class Car{
+    abstract void run();
+}
+
+public class Honda extends Car {
+    public void run(){
+        System.out.println("Car of Honda ");
+    }
+    public static void main(String args[]){
+        Honda h=new Honda();
+        h.run();
+    }
+    
+    
+}
+
